3722395cbee9af42bc51a2694460728970f33158
devtopher <christopher.david.mccoy@gmail.com>
2021-11-25 14:25:21 -0500

    Added open all button, added styling, fixed things

--- index.js
+++ index.js
@@ -1,70 +1,67 @@
-let myLeads = []
+let myLinks = []
+let billName = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLinks") )
 const billNameFromLocalStorage = JSON.parse( localStorage.getItem("billName") )
-
 const tabBtn = document.getElementById("tab-btn")
-let billName = []
+const openAllBtn = document.getElementById("open-all-btn")
 
+//Displays links and names from local storage
 if (leadsFromLocalStorage && billNameFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
+    myLinks = leadsFromLocalStorage
     billName = billNameFromLocalStorage
-    render(myLeads)
+    render(myLinks)
 }
 
+// Saves current tab with input as the text. Add alert if textbox is empty
 tabBtn.addEventListener("click", function(){ 
     billName.push(inputEl.value)
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    myLinks.push(tabs[0].url)
+    localStorage.setItem("myLinks", JSON.stringify(myLinks) )
     localStorage.setItem("billName", JSON.stringify(billName))
-    render(myLeads)
-    console.log(myLeads)
+    render(myLinks)
+    console.log(myLinks)
     })
 })
-
+//traverses array, pulling out each link name + link and putting it into ulEL
 function render(leads) {
+
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
         listItems += `
        
-       
+        <div class = "container">
             <li>
-            <div class = "container">
                 <a target='_blank' href='${leads[i]}'>
                     ${billName[i]}
                 </a>
-                <button class= "delete-btn-list">Delete</button>
             </li>
+           
             </div>
             
         
         `
     }
+    //<button id = "id${i}" class= "delete-btn-list">Delete</button>INSERT ABOVE IF IT EVER WORKS
     ulEl.innerHTML = listItems
 }
-// do i need to use a two value array in order to save the bill name as a callable thing.?
 
+// Clears local storage and empties arrays
 deleteBtn.addEventListener("click", function() {
-    console.log("clicked")
+    console.log("clicked clear")
     localStorage.clear()
-    myLeads = []
+    myLinks = []
     billName = []
-    render(myLeads)
+    render(myLinks)
 })
 
-// inputBtn.addEventListener("click", function() {
-//     myLeads.push(inputEl.value)
-//     inputEl.value = ""
-//     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-//     render(myLeads)})
-
-
-inputBtn.addEventListener("click", function(){
-    billName.push(inputEl.value)
-    
-
+openAllBtn.addEventListener("click", function() {
+    for (let i = 0; i<myLinks.length; i++ )
+    chrome.tabs.create({
+        url: myLinks[i]
+    })
 })
