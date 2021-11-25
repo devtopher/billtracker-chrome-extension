a03d23d662d40cb62377f561899d9ad3b48a3451
devtopher <christopher.david.mccoy@gmail.com>
2021-11-25 10:25:41 -0500

    added bill name functionality, delete button (not functional), local Storage for bill names

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-const tabBtn = document.getElementById("tab-btn")
+const billNameFromLocalStorage = JSON.parse( localStorage.getItem("billName") )
 
-if (leadsFromLocalStorage) {
+const tabBtn = document.getElementById("tab-btn")
+let billName = []
+
+if (leadsFromLocalStorage && billNameFromLocalStorage) {
     myLeads = leadsFromLocalStorage
+    billName = billNameFromLocalStorage
     render(myLeads)
 }
 
-tabBtn.addEventListener("click", function(){    
+tabBtn.addEventListener("click", function(){ 
+    billName.push(inputEl.value)
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    localStorage.setItem("billName", JSON.stringify(billName))
+    render(myLeads)
+    console.log(myLeads)
     })
 })
 
@@ -23,29 +30,41 @@ function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
         listItems += `
-       <div>
+       
        
             <li>
-            <input class="lower" type = "checkbox">
+            <div class = "container">
                 <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
+                    ${billName[i]}
                 </a>
+                <button class= "delete-btn-list">Delete</button>
             </li>
-        </div>
+            </div>
+            
+        
         `
     }
     ulEl.innerHTML = listItems
 }
+// do i need to use a two value array in order to save the bill name as a callable thing.?
 
 deleteBtn.addEventListener("click", function() {
+    console.log("clicked")
     localStorage.clear()
     myLeads = []
+    billName = []
     render(myLeads)
 })
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
+// inputBtn.addEventListener("click", function() {
+//     myLeads.push(inputEl.value)
+//     inputEl.value = ""
+//     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+//     render(myLeads)})
+
+
+inputBtn.addEventListener("click", function(){
+    billName.push(inputEl.value)
+    
+
 })
